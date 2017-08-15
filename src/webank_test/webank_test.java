15853dbeed938208544b2b075aee90eb088658e1
nanzou <nanzou@nanzou-NB0.webank.com>
2017-08-15 16:05:02 +0800

    this one branch

--- src/webank_test/webank_test.java
+++ src/webank_test/webank_test.java
@@ -3,5 +3,6 @@ package webank_test;
 public class webank_test {
 	public static void main(String args[]){
 		System.out.println("webank first test !");
+		System.out.println("this is one branch ... ");
 	}
 }
